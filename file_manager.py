6a2f5df51b8b095abbf2fe57b7fa528af00729de
Hiruni2000 <shhiruninawoda@gmail.com>
2023-10-08 19:42:24 +0530

    file mamager py

--- file_manager.py
+++ file_manager.py
@@ -1,6 +1,24 @@
+from config import mysql
+from flask import render_template
 
-import os
+mysql = mysql
+cursor = mysql.cursor(dictionary=True)
 
 
-host = os.environ.get('DB_HOST')
-print(host)
+def file_manager():
+    cursor.execute("SHOW TABLES")
+    table_info = []
+
+    for info in cursor.fetchall():
+        table_name = info['Tables_in_adbms']
+        cursor.execute(
+            f"SELECT table_name, ROUND(data_length / (1024 * 1024), 4) AS data_length_mb FROM "
+            f"information_schema.tables WHERE table_schema = 'adbms' AND table_name = '{table_name}'"
+        )
+        result = cursor.fetchone()
+        if result:
+            table_info.append(result)
+
+        # Add SQL here (each table row count) issue #1
+
+    return render_template('file-manager.html', table_info=table_info)
